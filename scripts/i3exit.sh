10415eacbac79356381806d897502b8a32ae6da2
erikdubois <erik.dubois@gmail.com>
2015-04-26 11:12:50 +0200

    Automatic Backup @ Sun Apr 26 11:12:50 CEST 2015

---
+++ scripts/i3exit.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+lock() {
+    #i3lock -c 222222
+    i3lock -i ~/.i3/scripts/lockscreen/lockscreen.png -t
+}
+
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        systemctl suspend
+        ;;
+    hibernate)
+        systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
