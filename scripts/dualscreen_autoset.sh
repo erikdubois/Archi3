fe85be51faa3401dbe040d188e044f6ca7eee361
erikdubois <erik.dubois@gmail.com>
2015-05-01 19:44:19 +0200

    Automatic Backup @ Fri May  1 19:44:19 CEST 2015

--- scripts/dualscreen_autoset.sh
+++ scripts/dualscreen_autoset.sh
@@ -2,14 +2,21 @@
 #
 # Dual monitor support
 
+if xrandr | grep -q 'DVI-I-0 connected' && xrandr | grep -q 'DVI-I-3 connected'
+then
+	xrandr --output DVI-I-0 --auto --left-of DVI-I-3
+
+fi
+
 if xrandr | grep -q 'DVI-I-1 connected' && xrandr | grep -q 'DVI-I-2 connected'
 then
 	xrandr --output DVI-I-1 --auto --left-of DVI-I-2
 
 fi
 
-if xrandr | grep -q 'DVI-I-0 connected' && xrandr | grep -q 'DVI-I-3 connected'
+
+if xrandr | grep -q 'DVI-I-2 connected' && xrandr | grep -q 'DVI-I-3 connected'
 then
-	xrandr --output DVI-I-0 --auto --left-of DVI-I-3
+	xrandr --output DVI-I-2 --auto --left-of DVI-I-3
 
 fi
