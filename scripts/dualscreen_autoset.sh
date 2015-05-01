6f02bf12e0a2a5f4409b2be66cc9749aa87f1c69
erikdubois <erik.dubois@gmail.com>
2015-05-01 22:53:52 +0200

    Automatic Backup @ Fri May  1 22:53:52 CEST 2015

--- scripts/dualscreen_autoset.sh
+++ scripts/dualscreen_autoset.sh
@@ -8,6 +8,13 @@ then
 
 fi
 
+if xrandr | grep -q 'DVI-0 connected' && xrandr | grep -q 'DVI-1 connected'
+then
+	xrandr --output DVI-0 --auto --left-of DVI-1
+
+fi
+
+
 if xrandr | grep -q 'DVI-I-1 connected' && xrandr | grep -q 'DVI-I-2 connected'
 then
 	xrandr --output DVI-I-1 --auto --left-of DVI-I-2
