609d1f39a06a413fb9e21c6b70721620ed8c1714
erikdubois <erik.dubois@gmail.com>
2015-05-14 21:07:54 +0200

    Automatic Backup @ Thu 14 May 21:07:54 CEST 2015

--- installation/install_i3_v16.sh
+++ installation/install_i3_v16.sh
@@ -400,10 +400,12 @@ packer screenfo --noedit
 
 # if you want a program that tells to update
 # or just type sudo pacman -Syu when you feel like it
-# packer pamac --noedit
+# packer pamac --noedit   # if not on antergos
 # execute pamac-tray in your config of i3
 # install also lxsession to be able to update and install in gui
 # sudo pacman -S lxsession
+
+sudo pacman -S pamac lxsession
 
         # T H U N A R #
 
