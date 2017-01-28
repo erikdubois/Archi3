a676d023d3f36c673d4be60bf512165969bfaaf4
Erik Dubois <erik.dubois@gmail.com>
2017-01-28 11:45:12 +0100

    Comment :  on 11:45:12 28-01-2017

--- installation/690-install-personal-settings-i3-configturation-v1.sh
+++ installation/690-install-personal-settings-i3-configturation-v1.sh
@@ -12,7 +12,7 @@ set -e
 ##################################################################################################################
 
 
-git clone https://github.com/erikdubois/archerik ~/.config/i3
+git clone https://github.com/erikdubois/archi3s ~/.config/i3
 
 echo "################################################################"
 echo "#########       personal settings installed     ################"
