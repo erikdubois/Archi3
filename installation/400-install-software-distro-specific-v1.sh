4ec58f0686cff6a3a5946a9fce79b3dc956244e6
Erik Dubois <erik.dubois@gmail.com>
2017-01-29 10:22:04 +0100

    Comment :  on 10:22:04 29-01-2017

--- installation/400-install-software-distro-specific-v1.sh
+++ installation/400-install-software-distro-specific-v1.sh
@@ -45,7 +45,7 @@ sudo pacman -S xorg-xrandr --noconfirm --needed
 sudo pacman -S gvfs  --noconfirm --needed
 sudo pacman -S compton  --noconfirm --needed
 sudo pacman -S volumeicon  --noconfirm --needed
-sudo packer -S arandr  --noconfirm 
+sudo packer -S arandr  --noconfirm --needed
 #run with systemadm
 sudo packer -S systemd-ui  --noconfirm --needed
 
