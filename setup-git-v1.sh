74d97869a3a7c906609bd4ee7fa1309f74c2e6f9
Erik Dubois <erik.dubois@gmail.com>
2017-02-04 13:33:26 +0100

    Comment :  on Sat Feb  4 13:33:26 CET 2017

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -5,16 +5,27 @@
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ##################################################################################################################
-# 
-
 ##################################################################################################################
 #
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
+# Problem solving commands
 
-#checking if git is installed else install it
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+
+# installing git if not installed for specific distro's
 
 if ! location="$(type -p "git")" || [ -z "git" ]; then
 
@@ -23,10 +34,28 @@ if ! location="$(type -p "git")" || [ -z "git" ]; then
 	echo "#################################################"
 
   	sudo apt install git -y
+	# check if apt-git is installed
+	if which apt-get > /dev/null; then
+
+		sudo apt-get install -y git
+
+	fi
+
+	# check if pacman is installed
+	if which pacman > /dev/null; then
+
+		sudo pacman -S --noconfirm git
+
+	fi
+
+	# check if eopkg is installed
+	if which eopkg > /dev/null; then
+
+		sudo eopkg -y install git
+
+	fi
+
 fi
-
-
-
 
 #setting up git
 #https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
@@ -36,11 +65,10 @@ git config --global user.name "Erik Dubois"
 git config --global user.email "erik.dubois@gmail.com"
 sudo git config --system core.editor nano
 git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
+git config --global credential.helper 'cache --timeout=18000'
 git config --global push.default simple
 
 
-
-
-
-echo " ALL  D O N E !"
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
