169909cfb18ae33a58000c8d94c9f631477b8e0b
Erik Dubois <erik.dubois@gmail.com>
2017-05-21 09:56:04 +0200

    Comment : xorg-apps on Sun May 21 09:56:04 CEST 2017

--- installation/030-install-xorg-intel.sh
+++ installation/030-install-xorg-intel.sh
@@ -15,7 +15,7 @@ echo "This is the opensource driver for nvidia"
 
 echo " Xserver setup"
 
-sudo pacman -S xorg-server xorg-server-utils xorg-xinit xorg-twm xterm --noconfirm --needed
+sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
 sudo pacman -S xf86-video-intel --noconfirm --needed
 
 echo "################################################################"
